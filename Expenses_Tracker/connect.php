aa8d09c726a2d6915fd5cd0e822731b09011d78e
Priya Kushwaha <99608708+Kushpriya@users.noreply.github.com>
2024-03-16 17:23:29 +0545

    Delete

--- Expenses_Tracker/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '@MYsql57';
-$database_name ='Expenses_tracker';
-
-$conn = mysqli_connect($hostname, $username, $password, $database_name);
-?>
