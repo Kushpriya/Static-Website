c66871853b10169fe09a0c8d6482d8c4e86635a2
Priya Kushwaha <99608708+Kushpriya@users.noreply.github.com>
2024-03-16 17:21:43 +0545

    Add files via upload

---
+++ Expenses_Tracker/connect.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = 'localhost';
+$username = 'root';
+$password = '@MYsql57';
+$database_name ='Expenses_tracker';
+
+$conn = mysqli_connect($hostname, $username, $password, $database_name);
+?>
